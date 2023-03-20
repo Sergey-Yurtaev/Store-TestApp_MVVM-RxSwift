5daeef46b59f325d98fbbedb4e1528f9168277d8
Sergey-Yurtaev <engineerk2@mail.ru>
2023-03-20 13:05:22 +0500

    Initial Commit

---
+++ Store-TestApp_MVVM-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Store-TestApp_MVVM-RxSwift
+//
+//  Created by  Sergey Yurtaev on 20.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
